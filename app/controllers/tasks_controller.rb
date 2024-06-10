0d4ba3873c5d3457f8313aaff4e9421262510c7c
RonanKer29 <kervella.ronan@hotmail.com>
2024-06-10 11:40:45 +0200

    CRUD actions ok

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,4 @@
 class TasksController < ApplicationController
-
   def index
     @tasks = Task.all
   end
@@ -21,10 +20,29 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
+  end
+
+ def destroy
+  @task = Task.find(params[:id])
+  @task.destroy
+  redirect_to tasks_path, status: :see_other
+end
+
+
   private
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
-
 end
